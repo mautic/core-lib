jason156/mautic

New API endpoint to create email reply entries

app/bundles/EmailBundle/Controller/Api/EmailApiController.php
@@ -11,17 +11,17 @@
 
 namespace Mautic\EmailBundle\Controller\Api;
 
+use Doctrine\ORM\EntityNotFoundException;
 use FOS\RestBundle\Util\Codes;
 use Mautic\ApiBundle\Controller\CommonApiController;
 use Mautic\CoreBundle\Helper\InputHelper;
+use Mautic\CoreBundle\Helper\RandomHelper\RandomHelperInterface;
+use Mautic\EmailBundle\MonitoredEmail\Processor\Reply;
 use Mautic\LeadBundle\Controller\LeadAccessTrait;
 use Mautic\LeadBundle\Entity\Lead;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
-/**
- * Class EmailApiController.
- */
 class EmailApiController extends CommonApiController
 {
     use LeadAccessTrait;
@@ -49,7 +49,7 @@ class EmailApiController extends CommonApiController
     /**
      * Obtains a list of emails.
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function getEntitiesAction()
     {
@@ -67,7 +67,7 @@ class EmailApiController extends CommonApiController
      *
      * @param int $id Email ID
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
@@ -106,7 +106,7 @@ class EmailApiController extends CommonApiController
      * @param int $id     Email ID
      * @param int $leadId Lead ID
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
@@ -165,5 +165,29 @@ class EmailApiController extends CommonApiController
         }
 
         return $this->notFound();
+    }
+
+    /**
+     * @param string $trackingHash
+     *
+     * @return Response
+     */
+    public function replyAction($trackingHash)
+    {
+        /** @var Reply $replyService */
+        $replyService = $this->get('mautic.message.processor.replier');
+
+        /** @var RandomHelperInterface $randomHelper */
+        $randomHelper = $this->get('mautic.helper.random');
+
+        try {
+            $replyService->createReplyByHash($trackingHash, "api-{$randomHelper->generate()}");
+        } catch (EntityNotFoundException $e) {
+            return $this->notFound($e->getMessage());
+        }
+
+        return $this->handleView(
+            $this->view(['success' => true], Codes::HTTP_CREATED)
+        );
     }
 }

app/bundles/EmailBundle/MonitoredEmail/Processor/Reply.php
@@ -11,6 +11,7 @@
 
 namespace Mautic\EmailBundle\MonitoredEmail\Processor;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Mautic\EmailBundle\EmailEvents;
 use Mautic\EmailBundle\Entity\EmailReply;
 use Mautic\EmailBundle\Entity\Stat;
@@ -53,13 +54,6 @@ class Reply implements ProcessorInterface
     private $logger;
 
     /**
-     * @var Message
-     */
-    private $message;
-
-    /**
-     * Reply constructor.
-     *
      * @param StatRepository           $statRepository
      * @param ContactFinder            $contactFinder
      * @param LeadModel                $leadModel
@@ -86,9 +80,7 @@ class Reply implements ProcessorInterface
      */
     public function process(Message $message)
     {
-        $this->message = $message;
-
-        $this->logger->debug('MONITORED EMAIL: Processing message ID '.$this->message->id.' for a reply');
+        $this->logger->debug('MONITORED EMAIL: Processing message ID '.$message->id.' for a reply');
 
         try {
             $parser       = new Parser($message);
@@ -120,33 +112,51 @@ class Reply implements ProcessorInterface
             return;
         }
 
-        $this->createReply($stat);
-
-        if ($this->dispatcher->hasListeners(EmailEvents::EMAIL_ON_REPLY)) {
-            $this->leadModel->setSystemCurrentLead($stat->getLead());
-
-            $event = new EmailReplyEvent($stat);
-            $this->dispatcher->dispatch(EmailEvents::EMAIL_ON_REPLY, $event);
-            unset($event);
-        }
+        $this->createReply($stat, $message->id);
+        $this->dispatchEvent($stat);
 
         $this->statRepo->clear();
         $this->leadModel->clearEntities();
     }
 
     /**
-     * @param Stat $stat
+     * @param string $trackingHash
+     * @param string $messageId
      */
-    protected function createReply(Stat $stat)
+    public function createReplyByHash($trackingHash, $messageId)
+    {
+        /** @var Stat|null $stat */
+        $stat = $this->statRepo->findOneBy(['trackingHash' => $trackingHash]);
+
+        if (null === $stat) {
+            throw new EntityNotFoundException("Email Stat with tracking hash {$trackingHash} was not found");
+        }
+
+        $stat->isRead(true);
+
+        $this->createReply($stat, $messageId);
+
+        $contact = $stat->getLead();
+
+        if ($contact) {
+            $this->dispatchEvent($stat);
+        }
+    }
+
+    /**
+     * @param Stat   $stat
+     * @param string $messageId
+     */
+    protected function createReply(Stat $stat, $messageId)
     {
         $replies = $stat->getReplies()->filter(
-            function (EmailReply $reply) {
-                return $reply->getMessageId() === $this->message->id;
+            function (EmailReply $reply) use ($messageId) {
+                return $reply->getMessageId() === $messageId;
             }
         );
 
         if (!$replies->count()) {
-            $emailReply = new EmailReply($stat, $this->message->id);
+            $emailReply = new EmailReply($stat, $messageId);
             $stat->addReply($emailReply);
             $this->statRepo->saveEntity($stat);
         }
@@ -162,5 +172,19 @@ class Reply implements ProcessorInterface
     protected function cleanEmail($email)
     {
         return strtolower(preg_replace("/[^a-z0-9\.@]/i", '', $email));
+    }
+
+    /**
+     * @param Stat $stat
+     */
+    private function dispatchEvent(Stat $stat)
+    {
+        if ($this->dispatcher->hasListeners(EmailEvents::EMAIL_ON_REPLY)) {
+            $this->leadModel->setSystemCurrentLead($stat->getLead());
+
+            $event = new EmailReplyEvent($stat);
+            $this->dispatcher->dispatch(EmailEvents::EMAIL_ON_REPLY, $event);
+            unset($event);
+        }
     }
 }

mautic/core-lib

bundles/EmailBundle/Controller/Api/EmailApiController.php
@@ -11,16 +11,16 @@
 
 namespace Mautic\EmailBundle\Controller\Api;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Mautic\ApiBundle\Controller\CommonApiController;
 use Mautic\CoreBundle\Helper\InputHelper;
+use Mautic\CoreBundle\Helper\RandomHelper\RandomHelperInterface;
+use Mautic\EmailBundle\MonitoredEmail\Processor\Reply;
 use Mautic\LeadBundle\Controller\LeadAccessTrait;
 use Mautic\LeadBundle\Entity\Lead;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 
-/**
- * Class EmailApiController.
- */
 class EmailApiController extends CommonApiController
 {
     use LeadAccessTrait;
@@ -48,7 +48,7 @@ class EmailApiController extends CommonApiController
     /**
      * Obtains a list of emails.
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function getEntitiesAction()
     {
@@ -66,7 +66,7 @@ class EmailApiController extends CommonApiController
      *
      * @param int $id Email ID
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
@@ -105,7 +105,7 @@ class EmailApiController extends CommonApiController
      * @param int $id     Email ID
      * @param int $leadId Lead ID
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
@@ -164,5 +164,29 @@ class EmailApiController extends CommonApiController
         }
 
         return $this->notFound();
+    }
+
+    /**
+     * @param string $trackingHash
+     *
+     * @return Response
+     */
+    public function replyAction($trackingHash)
+    {
+        /** @var Reply $replyService */
+        $replyService = $this->get('mautic.message.processor.replier');
+
+        /** @var RandomHelperInterface $randomHelper */
+        $randomHelper = $this->get('mautic.helper.random');
+
+        try {
+            $replyService->createReplyByHash($trackingHash, "api-{$randomHelper->generate()}");
+        } catch (EntityNotFoundException $e) {
+            return $this->notFound($e->getMessage());
+        }
+
+        return $this->handleView(
+            $this->view(['success' => true], Response::HTTP_CREATED)
+        );
     }
 }

bundles/EmailBundle/MonitoredEmail/Processor/Reply.php
@@ -11,6 +11,7 @@
 
 namespace Mautic\EmailBundle\MonitoredEmail\Processor;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Mautic\EmailBundle\EmailEvents;
 use Mautic\EmailBundle\Entity\EmailReply;
 use Mautic\EmailBundle\Entity\Stat;
@@ -54,18 +55,10 @@ class Reply implements ProcessorInterface
     private $logger;
 
     /**
-     * @var Message
-     */
-    private $message;
-
-    /**
      * @var ContactTracker
      */
     private $contactTracker;
 
-    /**
-     * Reply constructor.
-     */
     public function __construct(
         StatRepository $statRepository,
         ContactFinder $contactFinder,
@@ -84,9 +77,7 @@ class Reply implements ProcessorInterface
 
     public function process(Message $message)
     {
-        $this->message = $message;
-
-        $this->logger->debug('MONITORED EMAIL: Processing message ID '.$this->message->id.' for a reply');
+        $this->logger->debug('MONITORED EMAIL: Processing message ID '.$message->id.' for a reply');
 
         try {
             $parser       = new Parser($message);
@@ -118,30 +109,50 @@ class Reply implements ProcessorInterface
             return;
         }
 
-        $this->createReply($stat);
-
-        if ($this->dispatcher->hasListeners(EmailEvents::EMAIL_ON_REPLY)) {
-            $this->contactTracker->setSystemContact($stat->getLead());
-
-            $event = new EmailReplyEvent($stat);
-            $this->dispatcher->dispatch(EmailEvents::EMAIL_ON_REPLY, $event);
-            unset($event);
-        }
+        $this->createReply($stat, $message->id);
+        $this->dispatchEvent($stat);
 
         $this->statRepo->clear();
         $this->leadModel->clearEntities();
     }
 
-    protected function createReply(Stat $stat)
+    /**
+     * @param string $trackingHash
+     * @param string $messageId
+     */
+    public function createReplyByHash($trackingHash, $messageId)
+    {
+        /** @var Stat|null $stat */
+        $stat = $this->statRepo->findOneBy(['trackingHash' => $trackingHash]);
+
+        if (null === $stat) {
+            throw new EntityNotFoundException("Email Stat with tracking hash {$trackingHash} was not found");
+        }
+
+        $stat->isRead(true);
+
+        $this->createReply($stat, $messageId);
+
+        $contact = $stat->getLead();
+
+        if ($contact) {
+            $this->dispatchEvent($stat);
+        }
+    }
+
+    /**
+     * @param string $messageId
+     */
+    protected function createReply(Stat $stat, $messageId)
     {
         $replies = $stat->getReplies()->filter(
-            function (EmailReply $reply) {
-                return $reply->getMessageId() === $this->message->id;
+            function (EmailReply $reply) use ($messageId) {
+                return $reply->getMessageId() === $messageId;
             }
         );
 
         if (!$replies->count()) {
-            $emailReply = new EmailReply($stat, $this->message->id);
+            $emailReply = new EmailReply($stat, $messageId);
             $stat->addReply($emailReply);
             $this->statRepo->saveEntity($stat);
         }
@@ -157,5 +168,19 @@ class Reply implements ProcessorInterface
     protected function cleanEmail($email)
     {
         return strtolower(preg_replace("/[^a-z0-9\.@]/i", '', $email));
+    }
+
+    /**
+     * @param Stat $stat
+     */
+    private function dispatchEvent(Stat $stat)
+    {
+        if ($this->dispatcher->hasListeners(EmailEvents::EMAIL_ON_REPLY)) {
+            $this->contactTracker->setTrackedContact($stat->getLead());
+
+            $event = new EmailReplyEvent($stat);
+            $this->dispatcher->dispatch(EmailEvents::EMAIL_ON_REPLY, $event);
+            unset($event);
+        }
     }
 }