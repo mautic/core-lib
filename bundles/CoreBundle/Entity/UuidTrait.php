7ef0780c297a696cb26a7faacb805b91aa17ff30
Yash Khuthia <gunnrryy@gmail.com>
2020-12-23 11:21:18 +0000

    type hinting added to UuidInterface, loadMetadata function name changed to get called implicitly

--- bundles/CoreBundle/Entity/UuidTrait.php
+++ bundles/CoreBundle/Entity/UuidTrait.php
@@ -13,7 +13,7 @@ trait UuidTrait // @phpstan-ignore trait.unused (prepared for future use)
     #[Groups(['category:read', 'category:write', 'notification:read', 'notification:write', 'company:read', 'company:write', 'leadfield:read', 'leadfield:write', 'page:read', 'page:write', 'campaign:read', 'campaign:write', 'point:read', 'point:write', 'trigger:read', 'trigger:write', 'message:read', 'message:write', 'focus:read', 'focus:write', 'sms:read', 'sms:write', 'asset:read', 'asset:write', 'dynamicContent:read', 'dynamicContent:write', 'form:read', 'form:write', 'stage:read', 'stage:write', 'segment:read', 'segment:write', 'email:read', 'email:write'])]
     private ?string $uuid = null;
 
-    public static function addUuidField(ClassMetadataBuilder $builder): void
+    protected static function loadMetadata(ClassMetadataBuilder $builder)
     {
         $builder->createField('uuid', Types::GUID)
             ->nullable()
