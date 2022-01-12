4a9dbd4452297cbc2ed8a6992cfc22c4dd441dc4
Avikarsha Saha <avikarsha.saha@gmail.com>
2022-01-12 18:58:48 +0530

    phpstan fixes

--- bundles/LeadBundle/Event/GetStatDataEvent.php
+++ bundles/LeadBundle/Event/GetStatDataEvent.php
@@ -8,11 +8,13 @@ use Symfony\Component\EventDispatcher\Event;
 class GetStatDataEvent extends Event
 {
     /**
-     * @var array
+     * @var mixed[]
      */
     private $results = [];
 
     /**
+     * @param mixed[] $data
+     *
      * @throws KeyAlreadyExistsException
      */
     public function addResult(string $key, array $data): void
@@ -23,6 +25,9 @@ class GetStatDataEvent extends Event
         $this->results[$key] = $data;
     }
 
+    /**
+     * @return mixed[]
+     */
     public function getResults(): array
     {
         return $this->results;
