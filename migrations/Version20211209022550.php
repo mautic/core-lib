a757700b9eda945a7b66cdfcd393aa4f6d9cc767
Rahul Shinde <shinde.r.a@gmail.com>
2022-03-09 10:33:47 +0530

    Merge pull request #1674 from acquia/MAUT-6534-Migration
    MAUT-6534: Updated migration to update list only permission.
    (cherry picked from commit 3b314bbf8d935c65e6a4ffdddb9eab55ab152261)

--- migrations/Version20211209022550.php
+++ migrations/Version20211209022550.php
@@ -7,6 +7,7 @@ namespace Mautic\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 use Mautic\CoreBundle\Factory\ModelFactory;
+use Mautic\UserBundle\Entity\Permission;
 use Mautic\UserBundle\Entity\Role;
 use Mautic\UserBundle\Model\RoleModel;
 
@@ -61,10 +62,69 @@ final class Version20211209022550 extends AbstractMauticMigration
                 }
             }
 
-            $rawPermissions['lead:lists'] = array_unique($newPermissions);
+            $perms = array_unique($newPermissions);
 
-            $model->setRolePermissions($role, $rawPermissions);
-            $model->saveEntity($role);
+            $rawPermissions['lead:lists'] = $perms;
+
+            $bit = $this->getPermissionBitwise($perms);
+
+            // We have to get the segment permission to update the bitwise value.
+            // The rest of the permission will stay as-is.
+            $this->setBitwise($role, $bit, $rawPermissions);
         }
+    }
+
+    private function getPermissionBitwise(array $perms): int
+    {
+        $permBitwise = [
+            'viewown'     => 2,
+            'viewother'   => 4,
+            'editown'     => 8,
+            'editother'   => 16,
+            'create'      => 32,
+            'deleteown'   => 64,
+            'deleteother' => 128,
+            'full'        => 1024,
+        ];
+
+        $bit = 0;
+        foreach ($perms as $perm) {
+            $bit += $permBitwise[$perm];
+        }
+
+        return $bit;
+    }
+
+    /**
+     * @param mixed[] $rawPermissions
+     */
+    private function setBitwise(Role $role, int $bit, array $rawPermissions): void
+    {
+        $isPresent = false;
+        /** @var Permission $permission */
+        foreach ($role->getPermissions()->getIterator() as $permission) {
+            if ('lists' !== $permission->getName()) {
+                continue;
+            }
+            $isPresent = true;
+
+            $permission->setBitwise($bit);
+            $this->entityManager->persist($permission);
+            break;
+        }
+
+        if (!$isPresent) {
+            $permission = new Permission();
+            $permission->setBundle('lead');
+            $permission->setName('lists');
+            $permission->setBitwise($bit);
+            $this->entityManager->persist($permission);
+
+            $role->addPermission($permission);
+        }
+
+        $role->setRawPermissions($rawPermissions);
+        $this->entityManager->persist($role);
+        $this->entityManager->flush();
     }
 }
