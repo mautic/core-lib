023dffac1abd3849a15a55554c6a8da6a06f0ff9
Rahul Shinde <shinde.r.a@gmail.com>
2022-03-18 20:49:41 +0530

    MAUT-6960: Adjusted parameter in test call.

--- bundles/LeadBundle/Tests/Helper/ContactRequestHelperTest.php
+++ bundles/LeadBundle/Tests/Helper/ContactRequestHelperTest.php
@@ -205,7 +205,7 @@ class ContactRequestHelperTest extends \PHPUnit\Framework\TestCase
 
         $this->leadModel->expects($this->once())
             ->method('checkForDuplicateContact')
-            ->with($queryWithEmail, $this->trackedContact, true, true)
+            ->with($queryWithEmail, null, true, true)
             ->willReturn([$lead, ['email' => 'test@test.com']]);
 
         $helper = $this->getContactRequestHelper();
@@ -237,7 +237,7 @@ class ContactRequestHelperTest extends \PHPUnit\Framework\TestCase
 
         $this->leadModel->expects($this->once())
             ->method('checkForDuplicateContact')
-            ->with($query, $this->trackedContact, true, true)
+            ->with($query, null, true, true)
             ->willReturn([$this->trackedContact, []]);
 
         $helper = $this->getContactRequestHelper();
