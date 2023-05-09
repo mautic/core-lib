7e1835e81e1430e760e721d7237d0afb6d37eb08
Volha Pivavarchyk <volha@aivie.ch>
2023-05-09 16:55:07 +0300

    add test the case when an asset is downloaded by id

--- bundles/AssetBundle/Tests/Controller/PublicControllerFunctionalTest.php
+++ bundles/AssetBundle/Tests/Controller/PublicControllerFunctionalTest.php
@@ -4,6 +4,7 @@ namespace Mautic\AssetBundle\Tests\Controller;
 
 use Mautic\AssetBundle\Entity\Download;
 use Mautic\AssetBundle\Tests\Asset\AbstractAssetTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class PublicControllerFunctionalTest extends AbstractAssetTestCase
 {
@@ -44,6 +45,24 @@ class PublicControllerFunctionalTest extends AbstractAssetTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSame($this->expectedContentDisposition.$this->asset->getOriginalFileName(), $response->headers->get('Content-Disposition'));
         $this->assertEquals($this->expectedPngContent, $content);
+    }
+
+    /**
+     * Download action should return the file content.
+     */
+    public function testDownloadActionById(): void
+    {
+        $assetSlug = $this->asset->getId().':';
+
+        $this->client->request('GET', '/asset/'.$assetSlug.'?stream=0');
+        ob_start();
+        $response = $this->client->getResponse();
+        $response->sendContent();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $this->assertStringContainsString('404 Not Found', $content);
     }
 
     /**
