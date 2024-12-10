5f39c2a9b83cbf23726cface7e66398fbdee4cdc
aarohiprasad <aarohi.prasad@acquia.com>
2024-12-10 15:12:40 +0530

    test case complete

--- bundles/LeadBundle/Tests/Model/FieldModelDeleteTest.php
+++ bundles/LeadBundle/Tests/Model/FieldModelDeleteTest.php
@@ -11,90 +11,59 @@ use PHPUnit\Framework\Assert;
 
 class FieldModelDeleteTest extends MauticMysqlTestCase
 {
-    // copied creation and deletion and getColumns() from app/bundles/LeadBundle/Tests/Model/FieldModelTest.php
     private ?object $fieldModel;
     private LeadField $leadField;
-    private LeadField $leadField2;
+    private LeadField $companyField;
 
     public function setUp(): void
     {
-        $this->configParams['create_custom_field_in_background'] = $this->getName() === 'testBatchDeleteFields';
+        $this->configParams['create_custom_field_in_background'] = 'testBatchDeleteFields' === $this->getName();
 
         parent::setUp();
 
         $this->connection->beginTransaction();
 
         $this->fieldModel = self::$container->get('mautic.lead.model.field');
-    }
-    
-    public function testBatchCreateFields(): array
-    {
-        $leadField = new LeadField();
-        $leadField->setName('Test Lead Field')
+
+        $this->leadField = new LeadField();
+        $this->leadField->setName('Test Lead Field')
             ->setAlias('test_lead_field')
             ->setType('text')
             ->setObject('lead');
 
-        $leadField2 = new LeadField();
-        $leadField2->setName('Test Lead Field 2')
-            ->setAlias('test_lead_field2')
-            ->setType('text')
-            ->setObject('lead');
-
-        $companyField = new LeadField();
-        $companyField->setName('Test Company Field')
+        $this->companyField = new LeadField();
+        $this->companyField->setName('Test Company Field')
             ->setAlias('test_company_field')
             ->setType('text')
             ->setObject('company');
 
-        $companyField2 = new LeadField();
-        $companyField2->setName('Test Company Field 2')
-            ->setAlias('test_company_field2')
-            ->setType('text')
-            ->setObject('company');
+        try {
+            $this->fieldModel->saveEntity($this->leadField);
+        } catch (\Exception $e) {
+        }
 
-        $this->fieldModel->saveEntities([$leadField, $leadField2, $companyField, $companyField2]);
-        
-        $this->assertCount(1, $this->getColumns('leads', $leadField->getAlias()));
-        $this->assertCount(1, $this->getColumns('leads', $leadField2->getAlias()));
-        $this->assertCount(1, $this->getColumns('companies', $companyField->getAlias()));
-        $this->assertCount(1, $this->getColumns('companies', $companyField2->getAlias()));
-
-        return [$leadField, $leadField2, $companyField, $companyField2];
+        try {
+            $this->fieldModel->saveEntity($this->companyField);
+        } catch (\Exception $e) {
+        }
     }
 
-    /**
-     * @depends testBatchCreateFields
-     */
-    public function testBatchDeleteFields(array $leadFields)
+    public function testBatchDeleteFields(): void
     {
-        $this->assertCount(4, $leadFields);
+        $leadFieldRepository = $this->em->getRepository(LeadField::class);
+        \assert($leadFieldRepository instanceof LeadFieldRepository);
 
-        [$leadField, $leadField2, $companyField, $companyField2] = $leadFields;
+        $this->assertCount(45, $leadFieldRepository->findAll(), 'There should be 43 + 2 fields');
 
-//        $this->fieldModel->deleteEntities([$leadField->getId(), $leadField2->getId(), $companyField->getId(), $companyField2->getId()]);
-//
-//        $this->assertCount(0, $this->getColumns('leads', $leadField->getAlias()));
-//        $this->assertCount(0, $this->getColumns('leads', $leadField2->getAlias()));
-//        $this->assertCount(0, $this->getColumns('companies', $companyField->getAlias()));
-//        $this->assertCount(0, $this->getColumns('companies', $companyField2->getAlias()));
-//
         $jobRepository = $this->em->getRepository(Job::class);
         \assert($jobRepository instanceof JobRepository);
 
-        $leadColumnDeleteJobs = $jobRepository->findAll();
-        Assert::assertCount(4, $leadColumnDeleteJobs, 'There should be 4 jobs to delete lead column');
+        $leadColumnCreateJobs = $jobRepository->findBy(['task' => 'createLeadColumn']);
+        Assert::assertCount(2, $leadColumnCreateJobs, 'There should be 2 jobs to create lead column');
+
+        $this->fieldModel->deleteEntities([$this->leadField->getId(), $this->companyField->getId()]);
+
+        $leadColumnDeleteJobs = $jobRepository->findBy(['task' => 'deleteLeadColumn']);
+        Assert::assertCount(2, $leadColumnDeleteJobs, 'There should be 2 jobs to delete lead column');
     }
-
-    private function getColumns($table, $column)
-    {
-        $stmt = $this->connection->executeQuery(
-            "SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '{$this->connection->getDatabase()}' AND TABLE_NAME = '"
-            .MAUTIC_TABLE_PREFIX
-            ."$table' AND COLUMN_NAME = '$column'"
-        );
-
-        return $stmt->fetchAll();
-    }
-
 }
