818866fbd354a948acee99d76a9828fd2e9c0339
Rahul Shinde <shinde.r.a@gmail.com>
2025-01-06 13:54:09 +0530

    Updated global search for PointBundle

--- bundles/PointBundle/EventListener/SearchSubscriber.php
+++ bundles/PointBundle/EventListener/SearchSubscriber.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mautic\PointBundle\EventListener;
 
 use Mautic\CoreBundle\CoreEvents;
 use Mautic\CoreBundle\Event as MauticEvents;
 use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\LeadBundle\EventListener\GlobalSearchTrait;
 use Mautic\PointBundle\Model\PointModel;
 use Mautic\PointBundle\Model\TriggerModel;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -12,6 +15,8 @@ use Twig\Environment;
 
 class SearchSubscriber implements EventSubscriberInterface
 {
+    use GlobalSearchTrait;
+
     public function __construct(
         private PointModel $pointModel,
         private TriggerModel $pointTriggerModel,
@@ -23,90 +28,70 @@ class SearchSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            CoreEvents::GLOBAL_SEARCH      => ['onGlobalSearch', 0],
+            CoreEvents::GLOBAL_SEARCH => [
+                ['onGlobalSearchPointActions', 0],
+                ['onGlobalSearchPointTriggers', 0],
+            ],
             CoreEvents::BUILD_COMMAND_LIST => ['onBuildCommandList', 0],
         ];
     }
 
-    public function onGlobalSearch(MauticEvents\GlobalSearchEvent $event): void
+    public function onGlobalSearchPointActions(MauticEvents\GlobalSearchEvent $event)
     {
-        if ($this->security->isGranted('point:points:view')) {
-            $str = $event->getSearchString();
-            if (empty($str)) {
-                return;
-            }
-
-            $items = $this->pointModel->getEntities(
-                [
-                    'limit'  => 5,
-                    'filter' => $str,
-                ]);
-            $pointCount = count($items);
-            if ($pointCount > 0) {
-                $pointsResults = [];
-                $canEdit       = $this->security->isGranted('point:points:edit');
-                foreach ($items as $item) {
-                    $pointsResults[] = $this->twig->render(
-                        '@MauticPoint/SubscribedEvents/Search/global_point.html.twig',
-                        [
-                            'item'    => $item,
-                            'canEdit' => $canEdit,
-                        ]
-                    );
-                }
-                if ($pointCount > 5) {
-                    $pointsResults[] = $this->twig->render(
-                        '@MauticPoint/SubscribedEvents/Search/global_point.html.twig',
-                        [
-                            'showMore'     => true,
-                            'searchString' => $str,
-                            'remaining'    => ($pointCount - 5),
-                        ]
-                    );
-                }
-                $pointsResults['count'] = $pointCount;
-                $event->addResults('mautic.point.actions.header.index', $pointsResults);
-            }
+        if (!$this->security->isGranted('point:points:view')) {
+            return;
         }
 
-        if ($this->security->isGranted('point:triggers:view')) {
-            $str = $event->getSearchString();
-            if (empty($str)) {
-                return;
-            }
-
-            $items = $this->pointTriggerModel->getEntities(
-                [
-                    'limit'  => 5,
-                    'filter' => $str,
-                ]);
-            $count = count($items);
-            if ($count > 0) {
-                $results = [];
-                $canEdit = $this->security->isGranted('point:triggers:edit');
-                foreach ($items as $item) {
-                    $results[] = $this->twig->render(
-                        '@MauticPoint/SubscribedEvents/Search/global_trigger.html.twig',
-                        [
-                            'item'    => $item,
-                            'canEdit' => $canEdit,
-                        ]
-                    );
-                }
-                if ($count > 5) {
-                    $results[] = $this->twig->render(
-                        '@MauticPoint/SubscribedEvents/Search/global_trigger.html.twig',
-                        [
-                            'showMore'     => true,
-                            'searchString' => $str,
-                            'remaining'    => ($count - 5),
-                        ]
-                    );
-                }
-                $results['count'] = $count;
-                $event->addResults('mautic.point.trigger.header.index', $results);
-            }
+        $searchString = $event->getSearchString();
+        if (empty($searchString)) {
+            return;
         }
+
+        $items = $this->pointModel->getEntities([
+            'filter'           => $searchString,
+            'start'            => 0,
+            'limit'            => MauticEvents\GlobalSearchEvent::RESULTS_LIMIT,
+            'ignore_paginator' => true,
+            'with_total_count' => true,
+        ]);
+
+        $this->addGlobalSearchResults(
+            $this->twig,
+            $event,
+            $items,
+            'mautic.point.actions.header.index',
+            '@MauticPoint/SubscribedEvents/Search/global_point.html.twig',
+            ['canEdit' => $this->security->isGranted('point:points:edit')]
+        );
+    }
+
+    public function onGlobalSearchPointTriggers(MauticEvents\GlobalSearchEvent $event): void
+    {
+        if (!$this->security->isGranted('point:triggers:view')) {
+            return;
+        }
+
+        $searchString = $event->getSearchString();
+        if (empty($searchString)) {
+            return;
+        }
+
+        $items = $this->pointTriggerModel->getEntities([
+            'filter'           => $searchString,
+            'start'            => 0,
+            'limit'            => MauticEvents\GlobalSearchEvent::RESULTS_LIMIT,
+            'ignore_paginator' => true,
+            'with_total_count' => true,
+        ]);
+
+        $this->addGlobalSearchResults(
+            $this->twig,
+            $event,
+            $items,
+            'mautic.point.trigger.header.index',
+            '@MauticPoint/SubscribedEvents/Search/global_trigger.html.twig',
+            ['canEdit' => $this->security->isGranted('point:triggers:edit')]
+        );
     }
 
     public function onBuildCommandList(MauticEvents\CommandListEvent $event): void
