282f9f29a70fdba7c043095d3932c563eddf72a0
Rahul Shinde <shinde.r.a@gmail.com>
2025-01-12 06:28:32 +0530

    Refactored Dynamic Content global search

--- bundles/DynamicContentBundle/EventListener/SearchSubscriber.php
+++ bundles/DynamicContentBundle/EventListener/SearchSubscriber.php
@@ -5,24 +5,17 @@ declare(strict_types=1);
 namespace Mautic\DynamicContentBundle\EventListener;
 
 use Mautic\CoreBundle\CoreEvents;
+use Mautic\CoreBundle\DTO\GlobalSearchFilterDTO;
 use Mautic\CoreBundle\Event\GlobalSearchEvent;
-use Mautic\CoreBundle\Security\Permissions\CorePermissions;
+use Mautic\CoreBundle\Service\GlobalSearch;
 use Mautic\DynamicContentBundle\Model\DynamicContentModel;
-use Mautic\LeadBundle\EventListener\GlobalSearchTrait;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
 class SearchSubscriber implements EventSubscriberInterface
 {
-    use GlobalSearchTrait;
-
     public function __construct(
         private DynamicContentModel $dynamicContentModel,
-        private CorePermissions $security,
-        private Environment $twig,
+        private GlobalSearch $globalSearch,
     ) {
     }
 
@@ -33,40 +26,17 @@ class SearchSubscriber implements EventSubscriberInterface
         ];
     }
 
-    /**
-     * @throws SyntaxError
-     * @throws RuntimeError
-     * @throws LoaderError
-     */
     public function onGlobalSearch(GlobalSearchEvent $event): void
     {
-        $str = $event->getSearchString();
-        if (empty($str)) {
-            return;
-        }
-
-        $filter      = ['string' => $str, 'force' => ''];
-        $permissions = $this->security->isGranted(
-            ['dynamiccontent:dynamiccontents:viewown', 'dynamiccontent:dynamiccontents:viewother'],
-            'RETURN_ARRAY'
+        $filterDTO = new GlobalSearchFilterDTO($event->getSearchString());
+        $results   = $this->globalSearch->performSearch(
+            $filterDTO,
+            $this->dynamicContentModel,
+            '@MauticDynamicContent/SubscribedEvents/Search/global.html.twig'
         );
 
-        if ($permissions['dynamiccontent:dynamiccontents:viewown'] || $permissions['dynamiccontent:dynamiccontents:viewother']) {
-            $results = $this->dynamicContentModel->getEntities([
-                'start'             => 0,
-                'limit'             => GlobalSearchEvent::RESULTS_LIMIT,
-                'filter'            => $filter,
-                'with_total_count'  => true,
-                'ignore_paginator'  => true,
-            ]);
-
-            $this->addGlobalSearchResults(
-                $this->twig,
-                $event,
-                $results,
-                'mautic.dynamicContent.dynamicContent',
-                '@MauticDynamicContent/SubscribedEvents/Search/global.html.twig'
-            );
+        if (!empty($results)) {
+            $event->addResults('mautic.dynamicContent.dynamicContent', $results);
         }
     }
 }
