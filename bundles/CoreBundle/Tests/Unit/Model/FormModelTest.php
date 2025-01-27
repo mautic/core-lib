265b77d546fe8ae07e59d8d5f672549815702656
Rohit Pavaskar <rohit.pavaskar@acquia.com>
2025-01-27 10:57:23 +0530

    Make test class final

--- bundles/CoreBundle/Tests/Unit/Model/FormModelTest.php
+++ bundles/CoreBundle/Tests/Unit/Model/FormModelTest.php
@@ -11,7 +11,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\Event;
 
-class FormModelTest extends TestCase
+final class FormModelTest extends TestCase
 {
     /**
      * @var FormModel
