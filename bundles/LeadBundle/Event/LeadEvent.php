73ad9668a1de5cdb9eb00588f705959a83551629
putzwasser <26040044+putzwasser@users.noreply.github.com>
2025-02-26 16:26:42 +0100

    Fix: Adds missing return type to LeadEvent + unit test (#13842)
    * Fix: LeadEvent adds missing return type + unit test
    
    This adds the missing return type hint for `LeadEvent::getLead`. This also adds a simple unit test for `LeadEvent`
    
    * TASK: Removes comments
    
    * Improve grammar for unhide (#13835)
    
    * Improve grammar for unhide
    
    * Fix test
    
    * FIX: Reverts Constructor Property Promotion
    
    * Making the LeadEvent final and simplifying the tests that mocked it.
    
    * Rector's changes
    
    * CS fixes
    
    ---------
    
    Co-authored-by: Ruth Cheesley <ruth@ruthcheesley.co.uk>
    Co-authored-by: John Linhart <admin@escope.cz>
    Co-authored-by: John Linhart <jan@linhart.email>

--- bundles/LeadBundle/Event/LeadEvent.php
+++ bundles/LeadBundle/Event/LeadEvent.php
@@ -1,34 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mautic\LeadBundle\Event;
 
 use Mautic\CoreBundle\Event\CommonEvent;
 use Mautic\LeadBundle\Entity\Lead;
 
-class LeadEvent extends CommonEvent
+final class LeadEvent extends CommonEvent
 {
-    /**
-     * @param bool $isNew
-     */
-    public function __construct(Lead &$lead, $isNew = false)
-    {
-        $this->entity = &$lead;
+    public function __construct(
+        Lead $lead,
+        bool $isNew = false,
+    ) {
+        $this->entity = $lead;
         $this->isNew  = $isNew;
     }
 
-    /**
-     * Returns the Lead entity.
-     *
-     * @return Lead
-     */
-    public function getLead()
+    public function getLead(): Lead
     {
         return $this->entity;
     }
 
-    /**
-     * Sets the Lead entity.
-     */
     public function setLead(Lead $lead): void
     {
         $this->entity = $lead;
