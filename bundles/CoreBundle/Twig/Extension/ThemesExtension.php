f9abf49e814c7d14d6ebd918bdf9fbabf2e96921
Anderson, from Dropsolid <116097999+andersonjeccel@users.noreply.github.com>
2025-03-04 12:36:43 -0300

    Create ThemesExtension.php

---
+++ bundles/CoreBundle/Twig/Extension/ThemesExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\CoreBundle\Twig\Extension;
+
+use Mautic\CoreBundle\Helper\CoreParametersHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class ThemesExtension extends AbstractExtension
+{
+    public function __construct(private CoreParametersHelper $coreParametersHelper)
+    {
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('theme_text_on_color', [$this, 'theme_text_on_color']),
+            new TwigFunction('theme_text_on_color__helper', [$this, 'theme_text_on_color__helper']),
+        ];
+    }
+
+    public function theme_text_on_color(): string
+    {
+        $primaryColor = $this->coreParametersHelper->get('primary_brand_color', '000000');
+
+        $r = hexdec(substr($primaryColor, 0, 2));
+        $g = hexdec(substr($primaryColor, 2, 2));
+        $b = hexdec(substr($primaryColor, 4, 2));
+
+        // Calculate perceived brightness
+        $brightness = ($r * 299 + $g * 587 + $b * 114) / 1000;
+
+        // Determine text color based on brightness threshold
+        return $brightness > 125 ? '000000' : 'ffffff';
+    }
+
+    public function theme_text_on_color__helper(): string
+    {
+        return '000000' === $this->theme_text_on_color() ? '6d6d6d' : 'b3b3b3';
+    }
+}
