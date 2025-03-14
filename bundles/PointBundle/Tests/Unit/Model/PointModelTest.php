68fe2faea80f7c9f46fc989fc399d727b0782dcc
Zdeno Kuzmany <zdeno@kuzmany.biz>
2025-03-14 14:33:04 +0100

    refactor: Clean up test file and update method call signature

--- bundles/PointBundle/Tests/Unit/Model/PointModelTest.php
+++ bundles/PointBundle/Tests/Unit/Model/PointModelTest.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 
 declare(strict_types=1);
 
@@ -36,24 +35,24 @@ class PointModelTest extends TestCase
 
         // Create mock for the repository
         $this->pointRepository = $this->createMock(PointRepository::class);
-        
+
         // Create mock for IP lookup helper
         $this->ipLookupHelper = $this->createMock(IpLookupHelper::class);
-        
+
         // Create all other required mocks
-        $requestStack = $this->createMock(RequestStack::class);
-        $leadModel = $this->createMock(LeadModel::class);
-        $mauticFactory = $this->createMock(MauticFactory::class);
-        $contactTracker = $this->createMock(ContactTracker::class);
-        $em = $this->createMock(EntityManager::class);
-        $security = $this->createMock(CorePermissions::class);
-        $dispatcher = $this->createMock(EventDispatcherInterface::class);
-        $router = $this->createMock(UrlGeneratorInterface::class);
-        $translator = $this->createMock(Translator::class);
-        $userHelper = $this->createMock(UserHelper::class);
-        $logger = $this->createMock(\Psr\Log\LoggerInterface::class);
+        $requestStack         = $this->createMock(RequestStack::class);
+        $leadModel            = $this->createMock(LeadModel::class);
+        $mauticFactory        = $this->createMock(MauticFactory::class);
+        $contactTracker       = $this->createMock(ContactTracker::class);
+        $em                   = $this->createMock(EntityManager::class);
+        $security             = $this->createMock(CorePermissions::class);
+        $dispatcher           = $this->createMock(EventDispatcherInterface::class);
+        $router               = $this->createMock(UrlGeneratorInterface::class);
+        $translator           = $this->createMock(Translator::class);
+        $userHelper           = $this->createMock(UserHelper::class);
+        $logger               = $this->createMock(\Psr\Log\LoggerInterface::class);
         $coreParametersHelper = $this->createMock(CoreParametersHelper::class);
-        $pointGroupModel = $this->createMock(PointGroupModel::class);
+        $pointGroupModel      = $this->createMock(PointGroupModel::class);
 
         // Create a partial mock of the PointModel class
         $this->pointModel = $this->getMockBuilder(PointModel::class)
@@ -71,11 +70,11 @@ class PointModelTest extends TestCase
                 $userHelper,
                 $logger,
                 $coreParametersHelper,
-                $pointGroupModel
+                $pointGroupModel,
             ])
             ->onlyMethods(['getRepository'])
             ->getMock();
-        
+
         // Configure the getRepository method to return our repository mock
         $this->pointModel->method('getRepository')
             ->willReturn($this->pointRepository);
@@ -86,48 +85,46 @@ class PointModelTest extends TestCase
         // Arrange
         $type = 'email.send';
         $lead = new Lead();
-        
+
         // Configure repository to return empty array for getPublishedByType
         $this->pointRepository->expects($this->once())
             ->method('getPublishedByType')
             ->with($type)
             ->willReturn([]);
-            
+
         // IP lookup helper should not be called if we return early
         $this->ipLookupHelper->expects($this->never())
             ->method('getIpAddress');
-            
+
         // Act
-        $this->pointModel->triggerAction($type, null, null, $lead);
-        
+        $this->pointModel->triggerAction($type, null, null, $lead, true);
+
         // Assert is handled by expectations on mocks - test passes if ipLookupHelper is never called
     }
 
     public function testTriggerActionContinuesWhenPointsAreAvailable(): void
     {
         // Arrange
-        $type = 'email.send';
-        $lead = new Lead();
+        $type  = 'email.send';
+        $lead  = new Lead();
         $point = $this->createMock(Point::class);
-        
+
         // Configure repository to return a point for getPublishedByType
         $this->pointRepository->expects($this->once())
             ->method('getPublishedByType')
             ->with($type)
             ->willReturn([$point]);
-            
+
         // IP lookup helper should be called when points are available
         $this->ipLookupHelper->expects($this->once())
             ->method('getIpAddress');
-            
+
         // Act - wrap in try/catch since we're not mocking all dependencies
         try {
-            $this->pointModel->triggerAction($type, null, null, $lead);
+            $this->pointModel->triggerAction($type, null, null, $lead, true);
         } catch (\Exception $e) {
             // We're only testing that getPublishedByType is called and execution continues to getIpAddress
             // Other errors in the method execution are not relevant for this test
         }
-        
-        // Assert is handled by mock expectations
     }
 }
