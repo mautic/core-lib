500a3b97effd89a4789753bbcb4d90c30a251680
Jonas <j.ludwig@leuchtfeuer.com>
2025-03-26 14:24:56 +0100

    Better considering existing code logic

--- bundles/CampaignBundle/Controller/CampaignController.php
+++ bundles/CampaignBundle/Controller/CampaignController.php
@@ -1080,7 +1080,7 @@ class CampaignController extends AbstractStandardFormController
             /** @var LeadEventLogRepository $eventLogRepo */
             $eventLogRepo               = $this->doctrine->getManager()->getRepository(LeadEventLog::class);
             $campaignLogCounts          = $eventLogRepo->getCampaignLogCounts($id, false, false, true, $dateFrom, $dateToPlusOne);
-            $campaignLogCountsProcessed = $eventLogRepo->getCampaignLogCounts($id, false, false, false, $dateFrom, $dateToPlusOne);
+            $campaignLogCountsProcessed = $eventLogRepo->getCampaignLogCounts($id, true, false, false, $dateFrom, $dateToPlusOne);
         }
 
         return [
