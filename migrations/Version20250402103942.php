4e440f164d1d4238ba9269e3259870344006487e
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2025-04-09 07:41:19 -0300

    Atualizar o Version20250402103942.php
    Co-authored-by: John Linhart <jan@linhart.email>

--- migrations/Version20250402103942.php
+++ migrations/Version20250402103942.php
@@ -11,7 +11,7 @@ final class Version20250402103942 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE form_fields ADD field_width VARCHAR(255) DEFAULT \'100%\' NOT NULL');
+        $this->addSql('ALTER TABLE form_fields ADD field_width VARCHAR(50) DEFAULT \'100%\' NOT NULL');
     }
 
     public function down(Schema $schema): void
