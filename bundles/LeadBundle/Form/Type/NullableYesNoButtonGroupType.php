4fb25097269d51f45dcdbfb191be7a11c3013785
Martin Vooremäe <martin.vooremae@gmail.com>
2025-04-28 18:06:36 +0300

    Fix PHPStan error return.unusedType

--- bundles/LeadBundle/Form/Type/NullableYesNoButtonGroupType.php
+++ bundles/LeadBundle/Form/Type/NullableYesNoButtonGroupType.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class NullableYesNoButtonGroupType extends AbstractType
 {
-    public function getParent()
+    public function getParent(): string
     {
         return ButtonGroupType::class;
     }
