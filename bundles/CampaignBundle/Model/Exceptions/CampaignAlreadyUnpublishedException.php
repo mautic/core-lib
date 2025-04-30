7e9c8302b64ecdb25fc41b9ddad3dce61c9112be
Nilesh Lohar <nilesh.lohar@acquia.com>
2025-04-30 19:29:37 +0530

    PHPCS changes.

--- bundles/CampaignBundle/Model/Exceptions/CampaignAlreadyUnpublishedException.php
+++ bundles/CampaignBundle/Model/Exceptions/CampaignAlreadyUnpublishedException.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Mautic\CampaignBundle\Model\Exceptions;
 
-use Exception;
-
-class CampaignAlreadyUnpublishedException extends Exception
+class CampaignAlreadyUnpublishedException extends \Exception
 {
 }
--- bundles/CampaignBundle/Model/Exceptions/CampaignVersionMismatchedException.php
+++ bundles/CampaignBundle/Model/Exceptions/CampaignVersionMismatchedException.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Mautic\CampaignBundle\Model\Exceptions;
 
-use Exception;
-
-class CampaignVersionMismatchedException extends Exception
+class CampaignVersionMismatchedException extends \Exception
 {
 }
