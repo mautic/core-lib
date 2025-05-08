26c5169c9c7d8185a40338d2d80fd5d502ed9f99
Patryk Gruszka <patryk.gruszka@comarch.pl>
2025-05-08 17:25:51 +0200

    feat: [DPMMA-2816] event preview functional test

--- bundles/CampaignBundle/Tests/Controller/CampaignMetricsControllerFunctionalTest.php
+++ bundles/CampaignBundle/Tests/Controller/CampaignMetricsControllerFunctionalTest.php
@@ -6,7 +6,9 @@ namespace Mautic\CampaignBundle\Tests\Controller;
 
 use Mautic\CampaignBundle\Tests\Functional\Fixtures\FixtureHelper;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\EmailBundle\Entity\Stat;
 use Mautic\EmailBundle\Tests\Functional\Fixtures\EmailFixturesHelper;
+use Mautic\LeadBundle\Entity\Lead;
 use PHPUnit\Framework\Assert;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
@@ -133,5 +135,134 @@ class CampaignMetricsControllerFunctionalTest extends MauticMysqlTestCase
             Assert::assertEquals($expectedHoursData[$index]['label'], $dataset['label']);
             Assert::assertEquals($expectedHoursData[$index]['data'], $dataset['data']);
         }
+    }
+
+    public function testEventDetailsAction(): void
+    {
+        /** @var array<int, Lead> $contacts */
+        $contacts = [
+            $this->campaignFixturesHelper->createContact('john@example.com'),
+            $this->campaignFixturesHelper->createContact('paul@example.com'),
+            $this->campaignFixturesHelper->createContact('mawka@example.com'),
+            $this->campaignFixturesHelper->createContact('heksa@example.com'),
+        ];
+        $contacts[0]->setPoints(1);
+        $contacts[2]->setPoints(1);
+        $contacts[3]->setPoints(1);
+        $this->em->persist($contacts[0]);
+        $this->em->persist($contacts[2]);
+        $this->em->persist($contacts[3]);
+        $this->em->flush();
+
+        $email = $this->emailFixturesHelper->createEmail('Test Email');
+        $this->em->flush();
+        $emailId = $email->getId();
+
+        $emailLinks = [
+            $this->emailFixturesHelper->createEmailLink('https://example.com/1', $emailId),
+            $this->emailFixturesHelper->createEmailLink('https://example.com/2', $emailId),
+        ];
+        $this->em->flush();
+
+        $campaign      = $this->campaignFixturesHelper->createCampaignWithConditionalEmail($emailId);
+        foreach ($contacts as $contact) {
+            $this->campaignFixturesHelper->addContactToCampaign($contact, $campaign);
+        }
+        $this->em->flush();
+        $this->em->clear();
+
+        $events         = $campaign->getEvents();
+        $conditionEvent = $events->first();
+        $emailEvent     = $events->last();
+
+        // check condition event details before running the campaign
+        $this->client->request(Request::METHOD_GET, "/s/campaign/metrics/event-details/{$conditionEvent->getId()}");
+        $clientResponse = $this->client->getResponse();
+        $this->assertResponseIsSuccessful($clientResponse->getContent());
+        $conditionEventDetails = json_decode($clientResponse->getContent(), true);
+        $this->assertEquals([
+            'first_execution_date' => null,
+            'last_execution_date'  => null,
+            'total_executions'     => 0,
+            'pending_executions'   => 0,
+            'negative_path_count'  => 0,
+            'positive_path_count'  => 0,
+        ], $conditionEventDetails);
+
+        // check email event details before running the campaign
+        $this->client->request(Request::METHOD_GET, "/s/campaign/metrics/event-details/{$emailEvent->getId()}");
+        $clientResponse = $this->client->getResponse();
+        $this->assertResponseIsSuccessful($clientResponse->getContent());
+        $emailEventDetails = json_decode($clientResponse->getContent(), true);
+        $this->assertEquals([
+            'first_execution_date'      => null,
+            'last_execution_date'       => null,
+            'total_executions'          => 0,
+            'pending_executions'        => 0,
+            'sent_count'                => 0,
+            'read_count'                => 0,
+            'clicked_count'             => 0,
+            'open_rate'                 => '0%',
+            'click_through_rate'        => '0%',
+            'click_through_open_rate'   => '0%',
+        ], $emailEventDetails);
+
+        $commandResult = $this->testSymfonyCommand('mautic:campaigns:trigger', ['--campaign-id' => $campaign->getId()]);
+        Assert::assertStringContainsString('7 total events were executed', $commandResult->getDisplay());
+
+        // check condition event details after running the campaign
+        $this->client->request(Request::METHOD_GET, "/s/campaign/metrics/event-details/{$conditionEvent->getId()}");
+        $clientResponse = $this->client->getResponse();
+        $this->assertResponseIsSuccessful($clientResponse->getContent());
+        $conditionEventDetails = json_decode($clientResponse->getContent(), true);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $conditionEventDetails['first_execution_date']);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $conditionEventDetails['last_execution_date']);
+        $this->assertEquals(4, $conditionEventDetails['total_executions']);
+        $this->assertEquals(0, $conditionEventDetails['pending_executions']);
+        $this->assertEquals(1, $conditionEventDetails['negative_path_count']);
+        $this->assertEquals(3, $conditionEventDetails['positive_path_count']);
+
+        // check email event details after running the campaign
+        $this->client->request(Request::METHOD_GET, "/s/campaign/metrics/event-details/{$emailEvent->getId()}");
+        $clientResponse = $this->client->getResponse();
+        $this->assertResponseIsSuccessful($clientResponse->getContent());
+        $emailEventDetails = json_decode($clientResponse->getContent(), true);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $emailEventDetails['first_execution_date']);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $emailEventDetails['last_execution_date']);
+        $this->assertEquals(3, $emailEventDetails['total_executions']);
+        $this->assertEquals(0, $emailEventDetails['pending_executions']);
+        $this->assertEquals(3, $emailEventDetails['sent_count']);
+        $this->assertEquals(0, $emailEventDetails['read_count']);
+        $this->assertEquals(0, $emailEventDetails['clicked_count']);
+        $this->assertEquals('0%', $emailEventDetails['open_rate']);
+        $this->assertEquals('0%', $emailEventDetails['click_through_rate']);
+        $this->assertEquals('0%', $emailEventDetails['click_through_open_rate']);
+
+        // emulate email read and link click
+        $emailStats = $this->em->getRepository(Stat::class)->findBy(['email' => $email]);
+        $email      = $emailStats[0]->getEmail();
+        Assert::assertCount(3, $emailStats);
+        $this->emailFixturesHelper->emulateEmailRead($emailStats[0], $email);
+        $this->emailFixturesHelper->emulateEmailRead($emailStats[1], $email);
+        $this->em->flush();
+
+        $this->emailFixturesHelper->emulateLinkClick($email, $emailLinks[0], $emailStats[0]->getLead());
+        $this->em->flush();
+
+        // check email event details after emulating read and click
+        $this->client->request(Request::METHOD_GET, "/s/campaign/metrics/event-details/{$emailEvent->getId()}");
+        $clientResponse = $this->client->getResponse();
+        $this->assertResponseIsSuccessful($clientResponse->getContent());
+        $emailEventDetails = json_decode($clientResponse->getContent(), true);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $emailEventDetails['first_execution_date']);
+        $this->assertMatchesRegularExpression('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/', $emailEventDetails['last_execution_date']);
+        $this->assertEquals(3, $emailEventDetails['total_executions']);
+        $this->assertEquals(0, $emailEventDetails['pending_executions']);
+        $this->assertEquals(3, $emailEventDetails['sent_count']);
+        $this->assertEquals(2, $emailEventDetails['read_count']);
+        $this->assertEquals(1, $emailEventDetails['clicked_count']);
+        $this->assertEquals('66.67%', $emailEventDetails['open_rate']);
+        $this->assertEquals('33.33%', $emailEventDetails['click_through_rate']);
+        $this->assertEquals('50%', $emailEventDetails['click_through_open_rate']);
     }
 }
