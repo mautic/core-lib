3fa3113c526e5743bf3a629064ef62610b28cfe1
Hugo Prossaird <hpr@webmecanik.com>
2025-05-16 08:50:10 +0200

    revert: add back getIsCloned

--- bundles/LeadBundle/Entity/LeadField.php
+++ bundles/LeadBundle/Entity/LeadField.php
@@ -307,6 +307,11 @@ class LeadField extends FormEntity implements CacheInvalidateInterface
         return $this->id;
     }
 
+    public function getIsCloned(): bool
+    {
+        return $this->isCloned;
+    }
+
     /**
      * Set label.
      *
