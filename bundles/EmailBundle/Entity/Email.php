8dc1cd1b1481b1bde64386c1251c0c33f66ba8fa
Zdeno Kuzmany <zdeno@kuzmany.biz>
2025-06-30 11:24:05 +0200

    refactor: extract email name/subject max length to named constant
    - Add MAX_NAME_SUBJECT_LENGTH constant (190) to Email entity
    - Use constant in both name and subject Length constraints
    - Improves maintainability by eliminating magic number duplication
    
    Addresses Copilot AI feedback about duplicated magic numbers.

--- bundles/EmailBundle/Entity/Email.php
+++ bundles/EmailBundle/Entity/Email.php
@@ -61,6 +61,8 @@ class Email extends FormEntity implements VariantEntityInterface, TranslationEnt
     use DynamicContentEntityTrait;
     use UuidTrait;
 
+    public const MAX_NAME_SUBJECT_LENGTH = 190;
+
     /**
      * @var int
      *
@@ -446,7 +448,7 @@ class Email extends FormEntity implements VariantEntityInterface, TranslationEnt
             'name',
             new Length(
                 [
-                    'max'        => 190,
+                    'max'        => self::MAX_NAME_SUBJECT_LENGTH,
                     'maxMessage' => 'mautic.email.name.length',
                 ]
             )
@@ -465,7 +467,7 @@ class Email extends FormEntity implements VariantEntityInterface, TranslationEnt
             'subject',
             new Length(
                 [
-                    'max'        => 190,
+                    'max'        => self::MAX_NAME_SUBJECT_LENGTH,
                     'maxMessage' => 'mautic.email.subject.length',
                 ]
             )
