ec39a7bec8876a46e43af1b673f13f54811ac271
Zdeno Kuzmany <zdeno@kuzmany.biz>
2025-08-04 09:49:28 +0200

    Update app/bundles/ProjectBundle/DTO/EntityTypeConfig.php
    Co-authored-by: John Linhart <jan@linhart.email>

--- bundles/ProjectBundle/DTO/EntityTypeConfig.php
+++ bundles/ProjectBundle/DTO/EntityTypeConfig.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Mautic\ProjectBundle\DTO;
 
-readonly class EntityTypeConfig
+final readonly class EntityTypeConfig
 {
     public function __construct(
         public string $entityClass,
