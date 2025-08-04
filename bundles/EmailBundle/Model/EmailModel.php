d560af2786c8de079eb4a0dcdbe047234f0a892a
Lenon Leite <lenonleite@gmail.com>
2025-08-04 09:44:08 +0100

    Update app/bundles/EmailBundle/Model/EmailModel.php
    Co-authored-by: John Linhart <jan@linhart.email>

--- bundles/EmailBundle/Model/EmailModel.php
+++ bundles/EmailBundle/Model/EmailModel.php
@@ -2129,16 +2129,12 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface, GlobalSe
     }
 
     /**
-     * @param string              $type
-     * @param string              $filter
-     * @param int                 $limit
-     * @param int                 $start
      * @param array<string,mixed> $options
      *
      * @return array<string,array<string,string>>
      */
     public function getLookupResultsWithIdName(
-        $type, $filter = '', $limit = 10, $start = 0, $options = [],
+        string $type, string $filter = '', int $limit = 10, int $start = 0, array $options = [],
     ): array {
         $results    = $this->getLookupResults($type, $filter, $limit, $start, $options);
         $newResults = [];
