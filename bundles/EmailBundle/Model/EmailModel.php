28189d03f43694b44099dfbac923df3046d17c87
lenonleite <lenonleite@gmail.com>
2025-08-04 16:58:41 +0100

    - update doc

--- bundles/EmailBundle/Model/EmailModel.php
+++ bundles/EmailBundle/Model/EmailModel.php
@@ -2129,6 +2129,7 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface, GlobalSe
     }
 
     /**
+     * @param array<string> $filter
      * @param array<string,mixed> $options
      *
      * @return array<string,array<string,string>>
