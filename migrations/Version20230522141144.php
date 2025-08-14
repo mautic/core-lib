b699c87f634d327729b2dace514a19841a87dc64
Martin Vooremäe <martin.vooremae@gmail.com>
2025-08-14 09:11:06 +0300

    Fix skip assertion to run migration only if table exists

--- migrations/Version20230522141144.php
+++ migrations/Version20230522141144.php
@@ -9,17 +9,18 @@ use Mautic\CoreBundle\Doctrine\PreUpAssertionMigration;
 
 final class Version20230522141144 extends PreUpAssertionMigration
 {
+    protected const TABLE_NAME = 'plugin_citrix_events';
+
     protected function preUpAssertions(): void
     {
-        $this->skipAssertion(function (Schema $schema) {
-            return $schema->hasTable("{$this->prefix}plugin_citrix_events");
-        }, sprintf('Table %s already exists', "{$this->prefix}plugin_citrix_events"));
+        $this->skipAssertion(
+            fn (Schema $schema) => !$schema->hasTable($this->getPrefixedTableName()),
+            "Table {$this->getPrefixedTableName()} does not exist"
+        );
     }
 
     public function up(Schema $schema): void
     {
-        if ($schema->hasTable("{$this->prefix}plugin_citrix_events")) {
-            $schema->dropTable("{$this->prefix}plugin_citrix_events");
-        }
+        $schema->dropTable($this->getPrefixedTableName());
     }
 }
