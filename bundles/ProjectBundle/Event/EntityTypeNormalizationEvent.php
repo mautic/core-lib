dbf8d4c75793bd71b6afe11c80e8e597737ec2d6
Nilesh Lohar <nilesh.lohar@acquia.com>
2025-08-21 14:06:36 +0530

    MAUT-12922: Added Events for custom plugins entities.
    (cherry picked from commit b9584834627d707fcfb07162b98df9ff1a6e862a)

---
+++ bundles/ProjectBundle/Event/EntityTypeNormalizationEvent.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mautic\ProjectBundle\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * Event dispatched to allow bundles to extend entity type normalization mappings.
+ */
+class EntityTypeNormalizationEvent extends Event
+{
+    public const NAME = 'mautic.project.entity_type_normalization';
+
+    /** @var array<string, string> */
+    private array $mappings = [];
+
+    /**
+     * @param array<string, string> $defaultMappings
+     */
+    public function __construct(array $defaultMappings = [])
+    {
+        $this->mappings = $defaultMappings;
+    }
+
+    /**
+     * Add a normalization mapping.
+     */
+    public function addMapping(string $from, string $to): void
+    {
+        $this->mappings[$from] = $to;
+    }
+
+    /**
+     * Add multiple normalization mappings.
+     *
+     * @param array<string, string> $mappings
+     */
+    public function addMappings(array $mappings): void
+    {
+        $this->mappings = array_merge($this->mappings, $mappings);
+    }
+
+    /**
+     * Get all normalization mappings.
+     *
+     * @return array<string, string>
+     */
+    public function getMappings(): array
+    {
+        return $this->mappings;
+    }
+
+    /**
+     * Get normalized entity type or return original if no mapping exists.
+     */
+    public function getNormalizedType(string $entityType): string
+    {
+        return $this->mappings[$entityType] ?? $entityType;
+    }
+}
