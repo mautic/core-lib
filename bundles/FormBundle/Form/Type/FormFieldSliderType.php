af5a886ed2e19326887fe3335135afc0483df0e3
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2025-08-27 11:05:27 -0300

    phpunit

--- bundles/FormBundle/Form/Type/FormFieldSliderType.php
+++ bundles/FormBundle/Form/Type/FormFieldSliderType.php
@@ -7,23 +7,44 @@ namespace Mautic\FormBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraint;
 
 /**
- * @extends AbstractType<FormFieldSliderType>
+ * Custom IntegerType that supports constraints
+ */
+final class ConstrainedIntegerType extends IntegerType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+        
+        $resolver->setDefined('constraints');
+        $resolver->setAllowedTypes('constraints', ['array']);
+        $resolver->setAllowedValues('constraints', function ($constraints) {
+            if (!is_array($constraints)) {
+                return false;
+            }
+            
+            foreach ($constraints as $constraint) {
+                if (!$constraint instanceof Constraint) {
+                    return false;
+                }
+            }
+            
+            return true;
+        });
+    }
+}
+
+/**
+ * @extends AbstractType<mixed>
  */
 final class FormFieldSliderType extends AbstractType
 {
-    public function __construct(
-        private ?TranslatorInterface $translator = null
-    ) {
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('min', IntegerType::class, [
+        $builder->add('min', ConstrainedIntegerType::class, [
             'label'      => 'mautic.form.field.form.slider_min',
             'label_attr' => ['class' => 'control-label'],
             'required'   => false,
@@ -31,54 +52,36 @@ final class FormFieldSliderType extends AbstractType
             'data'       => $options['data']['min'] ?? 0,
         ]);
 
-        $builder->add('max', IntegerType::class, [
+        $builder->add('max', ConstrainedIntegerType::class, [
             'label'      => 'mautic.form.field.form.slider_max',
             'label_attr' => ['class' => 'control-label'],
             'required'   => false,
             'attr'       => ['class' => 'form-control'],
             'data'       => $options['data']['max'] ?? 100,
+            'constraints' => [
+                new \Symfony\Component\Validator\Constraints\Expression([
+                    'expression' => 'this.getParent()["min"].getData() < value',
+                    'message' => 'mautic.form.field.form.slider_max_gt_min_error'
+                ])
+            ]
         ]);
 
-        $builder->add('step', IntegerType::class, [
+        $builder->add('step', ConstrainedIntegerType::class, [
             'label'      => 'mautic.form.field.form.slider_step',
             'label_attr' => ['class' => 'control-label'],
             'required'   => false,
             'attr'       => ['class' => 'form-control'],
             'data'       => $options['data']['step'] ?? 1,
+            'constraints' => [
+                new \Symfony\Component\Validator\Constraints\Range([
+                    'min' => 1,
+                    'minMessage' => 'mautic.form.field.form.slider_step_min_error'
+                ]),
+                new \Symfony\Component\Validator\Constraints\Expression([
+                    'expression' => 'value < this.getParent()["max"].getData()',
+                    'message' => 'mautic.form.field.form.slider_step_lt_max_error'
+                ])
+            ]
         ]);
-
-        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
-            $form = $event->getForm();
-            $data = $event->getData();
-
-            if (!$data) {
-                return;
-            }
-
-            $min = $data['min'] ?? 0;
-            $max = $data['max'] ?? 100;
-            $step = $data['step'] ?? 1;
-
-            if ($max <= $min) {
-                $message = $this->translator 
-                    ? $this->translator->trans('mautic.form.field.form.slider_max_gt_min_error', [], 'validators')
-                    : 'mautic.form.field.form.slider_max_gt_min_error';
-                $form->get('max')->addError(new \Symfony\Component\Form\FormError($message));
-            }
-
-            if ($step < 1) {
-                $message = $this->translator 
-                    ? $this->translator->trans('mautic.form.field.form.slider_step_min_error', [], 'validators')
-                    : 'mautic.form.field.form.slider_step_min_error';
-                $form->get('step')->addError(new \Symfony\Component\Form\FormError($message));
-            }
-
-            if ($step >= $max) {
-                $message = $this->translator 
-                    ? $this->translator->trans('mautic.form.field.form.slider_step_lt_max_error', [], 'validators')
-                    : 'mautic.form.field.form.slider_step_lt_max_error';
-                $form->get('step')->addError(new \Symfony\Component\Form\FormError($message));
-            }
-        });
     }
 }
