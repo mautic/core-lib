10ab6827b3c61fa8688931701e684c1aaab863be
Avikarsha Saha <avikarsha.saha@acquia.com>
2025-09-12 13:21:41 +0530

    Refactor test

--- bundles/EmailBundle/Tests/Controller/EmailGraphStatsControllerFunctionalTest.php
+++ bundles/EmailBundle/Tests/Controller/EmailGraphStatsControllerFunctionalTest.php
@@ -3,6 +3,7 @@
 namespace Mautic\EmailBundle\Tests\Controller;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
+use Mautic\CoreBundle\Tests\Functional\CreateTestEntitiesTrait;
 use Mautic\EmailBundle\Entity\Email;
 use Mautic\LeadBundle\Entity\LeadList;
 use PHPUnit\Framework\Assert;
@@ -10,10 +11,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 class EmailGraphStatsControllerFunctionalTest extends MauticMysqlTestCase
 {
+    use CreateTestEntitiesTrait;
+
     public function testTemplateViewAction(): void
     {
-        $email   = $this->createEmail('Email A', 'Email A Subject', 'template', 'beefree-empty', 'Test html');
-        $this->em->flush();
+        $email = $this->createAndPersistEmail('Email A');
 
         $this->client->request(Request::METHOD_GET, "/s/emails-graph-stats/{$email->getId()}/0/2022-08-21/2022-09-21");
         Assert::assertTrue($this->client->getResponse()->isOk());
@@ -21,43 +23,21 @@ class EmailGraphStatsControllerFunctionalTest extends MauticMysqlTestCase
 
     public function testSegmentViewAction(): void
     {
-        $segment        = $this->createSegment('Segment B', 'segment-B');
-        $email          = $this->createEmail('Email B', 'Email B Subject', 'list', 'beefree-empty', 'Test html', $segment);
-        $this->em->flush();
+        $segment = $this->createSegment('segment-B', []);
+        $email   = $this->createAndPersistEmail('Email B', $segment);
 
         $this->client->request(Request::METHOD_GET, "/s/emails-graph-stats/{$email->getId()}/0/2022-08-21/2022-09-21");
         Assert::assertTrue($this->client->getResponse()->isOk());
     }
 
-    private function createSegment(string $name, string $alias): LeadList
+    private function createAndPersistEmail(string $name, ?LeadList $segment = null): Email
     {
-        $segment = new LeadList();
-        $segment->setName($name);
-        $segment->setAlias($alias);
-        $segment->setPublicName($name);
-        $this->em->persist($segment);
-
-        return $segment;
-    }
-
-    /**
-     * @param array<mixed>|null $varientSetting
-     *
-     * @throws \Doctrine\ORM\ORMException
-     */
-    private function createEmail(string $name, string $subject, string $emailType, string $template, string $customHtml, ?LeadList $segment = null, ?array $varientSetting = []): Email
-    {
-        $email = new Email();
-        $email->setName($name);
-        $email->setSubject($subject);
-        $email->setEmailType($emailType);
-        $email->setTemplate($template);
-        $email->setCustomHtml($customHtml);
-        $email->setVariantSettings($varientSetting);
-        if (!empty($segment)) {
+        $email = $this->createEmail($name);
+        if (null !== $segment) {
             $email->addList($segment);
         }
         $this->em->persist($email);
+        $this->em->flush();
 
         return $email;
     }
