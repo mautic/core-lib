5064c98012e11583abbbb090b48a02d8b8cb5d07
John Linhart <jan@linhart.email>
2025-10-10 18:02:10 +0200

    CS fix

--- bundles/LeadBundle/Model/FieldModel.php
+++ bundles/LeadBundle/Model/FieldModel.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Mautic\CoreBundle\Cache\ResultCacheOptions;
 use Mautic\CoreBundle\Doctrine\Helper\ColumnSchemaHelper;
-use Mautic\CoreBundle\Doctrine\Paginator\SimplePaginator;
 use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Mautic\CoreBundle\Helper\InputHelper;
 use Mautic\CoreBundle\Helper\UserHelper;
