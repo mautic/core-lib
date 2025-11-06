68d6363d884cca2199902e7c795502571dbbd155
Rahul Shinde <shinde.r.a@gmail.com>
2025-11-06 08:32:21 +0530

    CS fixes

--- bundles/EmailBundle/Model/EmailModel.php
+++ bundles/EmailBundle/Model/EmailModel.php
@@ -2324,16 +2324,11 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface, GlobalSe
         return $url;
     }
 
-
-
     /**
      * Check if an email is a variant by looking it up in emailSettings.
      * Handles both parent emails and translations (translations inherit variant status from parent).
      *
-     * @param int   $emailId
-     * @param array $emailSettings
-     *
-     * @return bool
+     * @param array<int, array<string, mixed>> $emailSettings
      */
     private function isEmailVariant(int $emailId, array $emailSettings): bool
     {
