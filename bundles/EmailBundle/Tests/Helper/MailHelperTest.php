e1fb08905abbc6dd62cf2fb0377ede1aaeec7ba7
Alexander Petermann <alex@petermann.dev>
2025-11-29 23:39:05 +0100

    fix: ci test, we do set the sender explicitly, now

--- bundles/EmailBundle/Tests/Helper/MailHelperTest.php
+++ bundles/EmailBundle/Tests/Helper/MailHelperTest.php
@@ -1423,6 +1423,7 @@ class MailHelperTest extends TestCase
             [
                 'To'                    => 'contact1@somewhere.com',
                 'From'                  => 'No Body <nobody@nowhere.com>',
+                'Sender'                => 'No Body <nobody@nowhere.com>',
                 'Reply-To'              => 'nobody@nowhere.com',
                 'Subject'               => 'Test',
                 'X-Mautic-Test-2'       => MailHelper::getBlankPixel(),
