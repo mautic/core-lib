eddb0bfd4febe524062e05739d5604b646cab2dc
John Linhart <admin@escope.cz>
2025-12-03 13:51:36 +0100

    Merge pull request #2855 from acquia/MAUT-13267
    MAUT-13267 Fix: parse_str is converting dots to underscores

--- bundles/CoreBundle/Helper/UrlHelper.php
+++ bundles/CoreBundle/Helper/UrlHelper.php
@@ -2,6 +2,8 @@
 
 namespace Mautic\CoreBundle\Helper;
 
+use GuzzleHttp\Psr7\Query;
+
 class UrlHelper
 {
     /**
@@ -224,11 +226,11 @@ class UrlHelper
         $query = parse_url($url, PHP_URL_QUERY);
 
         if (!empty($query)) {
-            parse_str($query, $parsedQuery);
+            $parsedQuery = Query::parse($query);
 
             if ($parsedQuery) {
-                $encodedQuery = http_build_query($parsedQuery, '', null, PHP_QUERY_RFC3986);
-                $url          = str_replace('?'.$query, '?'.$encodedQuery, $url);
+                $encodedQuery = Query::build($parsedQuery, PHP_QUERY_RFC3986);
+                $url          = str_replace($query, $encodedQuery, $url);
             }
         }
 
