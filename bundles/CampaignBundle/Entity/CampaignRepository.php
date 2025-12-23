02b6a0886688d03580c9f9d8ee4ac19372bb936c
Rahul Shinde <shinde.r.a@gmail.com>
2025-12-23 09:06:07 +0530

    refactor: move campaign email lookup to EventRepository
    Refactors campaign email fetching to a single query in EventRepository, reducing duplicate DB calls. Deprecates the legacy CampaignRepository method in preparation for removal in Mautic 8.x.

--- bundles/CampaignBundle/Entity/CampaignRepository.php
+++ bundles/CampaignBundle/Entity/CampaignRepository.php
@@ -641,7 +641,12 @@ class CampaignRepository extends CommonRepository
      * or empty array if nothing found.
      *
      * @param int $id
+     *
+     * @deprecated The method is deprecated and will be removed in Mautic 8.x.
+     * Use `\Mautic\CampaignBundle\Entity\EventRepository::getCampaignEmailEvents` instead.
+     * @see EventRepository::getCampaignEmailEvents
      */
+    #[\Deprecated('The method is deprecated and will be removed in Mautic 8.x.')]
     public function fetchEmailIdsById($id): array
     {
         $emails = $this->getEntityManager()
