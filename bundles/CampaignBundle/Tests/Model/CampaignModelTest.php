d8f39cf38fb3fe56402495eb5ff3f7452dcad903
Rahul Shinde <shinde.r.a@gmail.com>
2026-03-24 16:00:11 +0530

    test(campaign): add coverage for normalized source persistence

--- bundles/CampaignBundle/Tests/Model/CampaignModelTest.php
+++ bundles/CampaignBundle/Tests/Model/CampaignModelTest.php
@@ -4,6 +4,7 @@ namespace Mautic\CampaignBundle\Tests\Model;
 
 use Mautic\CampaignBundle\Entity\Campaign;
 use Mautic\CampaignBundle\Tests\CampaignTestAbstract;
+use Mautic\FormBundle\Entity\Form;
 use Mautic\LeadBundle\Entity\LeadList;
 
 class CampaignModelTest extends CampaignTestAbstract
@@ -62,5 +63,80 @@ class CampaignModelTest extends CampaignTestAbstract
             ->method('addList');
 
         $model->setLeadSources($campaign, ['lists' => ['list-one' => 0]], []);
+    }
+
+    public function testSetLeadSourcesAddsFormById(): void
+    {
+        $model    = $this->initCampaignModel();
+        $campaign = $this->createMock(Campaign::class);
+        $form     = $this->createMock(Form::class);
+
+        $this->entityManager->expects($this->once())
+            ->method('find')
+            ->with(Form::class, parent::$mockId)
+            ->willReturn($form);
+
+        $campaign->expects($this->once())
+            ->method('addForm')
+            ->with($form);
+
+        $model->setLeadSources($campaign, ['forms' => [parent::$mockId => parent::$mockName]], []);
+    }
+
+    public function testSetLeadSourcesRemovesLeadListById(): void
+    {
+        $model    = $this->initCampaignModel();
+        $campaign = $this->createMock(Campaign::class);
+        $leadList = $this->createMock(LeadList::class);
+
+        $this->entityManager->expects($this->once())
+            ->method('find')
+            ->with(LeadList::class, parent::$mockId)
+            ->willReturn($leadList);
+
+        $campaign->expects($this->once())
+            ->method('removeList')
+            ->with($leadList);
+
+        $model->setLeadSources($campaign, [], ['lists' => [parent::$mockId => parent::$mockName]]);
+    }
+
+    public function testSetLeadSourcesRemovesFormById(): void
+    {
+        $model    = $this->initCampaignModel();
+        $campaign = $this->createMock(Campaign::class);
+        $form     = $this->createMock(Form::class);
+
+        $this->entityManager->expects($this->once())
+            ->method('find')
+            ->with(Form::class, parent::$mockId)
+            ->willReturn($form);
+
+        $campaign->expects($this->once())
+            ->method('removeForm')
+            ->with($form);
+
+        $model->setLeadSources($campaign, [], ['forms' => [parent::$mockId => parent::$mockName]]);
+    }
+
+    public function testSetLeadSourcesIgnoresNonNumericFormIdentifier(): void
+    {
+        $model    = $this->initCampaignModel();
+        $campaign = $this->createMock(Campaign::class);
+
+        $this->entityManager->expects($this->never())
+            ->method('find');
+
+        $campaign->expects($this->never())
+            ->method('addForm');
+
+        $campaign->expects($this->never())
+            ->method('removeForm');
+
+        $model->setLeadSources(
+            $campaign,
+            ['forms' => ['form-one' => 0]],
+            ['forms' => ['form-two' => 0]]
+        );
     }
 }
