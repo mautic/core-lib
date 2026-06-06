9ef3022e5392cbc90f481aa2de33a57c38558ad1
Anderson J. Eccel <116097999+andersonjeccel@users.noreply.github.com>
2026-06-05 21:46:57 -0300

    reduce field template test duplication

--- bundles/FormBundle/Tests/Twig/FieldTemplateTest.php
+++ bundles/FormBundle/Tests/Twig/FieldTemplateTest.php
@@ -6,7 +6,6 @@ namespace Mautic\FormBundle\Tests\Twig;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\FormBundle\Entity\Field;
-use Mautic\FormBundle\Entity\Form;
 
 final class FieldTemplateTest extends MauticMysqlTestCase
 {
@@ -15,29 +14,7 @@ final class FieldTemplateTest extends MauticMysqlTestCase
 
     public function testFieldTemplateRendersWithCssClasses(): void
     {
-        $field = new Field();
-        $field->setType('text');
-        $field->setLabel(self::FIELD_LABEL);
-        $field->setAlias('test_field');
-        $field->setFieldWidth('50%');
-        $field->setOrder(1);
-
-        $form = new Form();
-        $form->setName('Test Form');
-        $form->setAlias('test_form');
-
-        $twig     = $this->getContainer()->get('twig');
-        $template = $twig->load(self::TEXT_FIELD_TEMPLATE);
-
-        $html = $template->render([
-            'field'          => $field,
-            'fields'         => [],
-            'id'             => 'test_id',
-            'formName'       => 'test_form',
-            'containerClass' => 'text',
-            'type'           => 'text',
-            'inputClass'     => 'input',
-        ]);
+        $html = $this->renderTextField($this->createField('50%'));
 
         $this->assertStringContainsString('mauticform-50', $html);
         $this->assertStringNotContainsString('style="width: 50%"', $html);
@@ -45,28 +22,7 @@ final class FieldTemplateTest extends MauticMysqlTestCase
 
     public function testFieldTemplateRendersFullWidthByDefault(): void
     {
-        $field = new Field();
-        $field->setType('text');
-        $field->setLabel(self::FIELD_LABEL);
-        $field->setAlias('test_field');
-        $field->setOrder(1);
-
-        $form = new Form();
-        $form->setName('Test Form');
-        $form->setAlias('test_form');
-
-        $twig     = $this->getContainer()->get('twig');
-        $template = $twig->load(self::TEXT_FIELD_TEMPLATE);
-
-        $html = $template->render([
-            'field'          => $field,
-            'fields'         => [],
-            'id'             => 'test_id',
-            'formName'       => 'test_form',
-            'containerClass' => 'text',
-            'type'           => 'text',
-            'inputClass'     => 'input',
-        ]);
+        $html = $this->renderTextField($this->createField());
 
         $this->assertStringContainsString('mauticform-100', $html);
     }
@@ -83,28 +39,41 @@ final class FieldTemplateTest extends MauticMysqlTestCase
         ];
 
         foreach ($widthMappings as $percentage => $expectedClass) {
-            $field = new Field();
-            $field->setType('text');
-            $field->setLabel(self::FIELD_LABEL);
-            $field->setAlias('test_field');
-            $field->setFieldWidth($percentage);
-            $field->setOrder(1);
-
-            $twig     = $this->getContainer()->get('twig');
-            $template = $twig->load(self::TEXT_FIELD_TEMPLATE);
-
-            $html = $template->render([
-                'field'          => $field,
-                'fields'         => [],
-                'id'             => 'test_id',
-                'formName'       => 'test_form',
-                'containerClass' => 'text',
-                'type'           => 'text',
-                'inputClass'     => 'input',
-            ]);
+            $html = $this->renderTextField($this->createField($percentage));
 
             $this->assertStringContainsString($expectedClass, $html, "Field width {$percentage} should map to class {$expectedClass}");
             $this->assertStringNotContainsString("style=\"width: {$percentage}\"", $html, "Field width {$percentage} should not have inline style");
         }
+    }
+
+    private function createField(?string $fieldWidth = null): Field
+    {
+        $field = new Field();
+        $field->setType('text');
+        $field->setLabel(self::FIELD_LABEL);
+        $field->setAlias('test_field');
+        $field->setOrder(1);
+
+        if (null !== $fieldWidth) {
+            $field->setFieldWidth($fieldWidth);
+        }
+
+        return $field;
+    }
+
+    private function renderTextField(Field $field): string
+    {
+        $twig     = $this->getContainer()->get('twig');
+        $template = $twig->load(self::TEXT_FIELD_TEMPLATE);
+
+        return $template->render([
+            'field'          => $field,
+            'fields'         => [],
+            'id'             => 'test_id',
+            'formName'       => 'test_form',
+            'containerClass' => 'text',
+            'type'           => 'text',
+            'inputClass'     => 'input',
+        ]);
     }
 }
